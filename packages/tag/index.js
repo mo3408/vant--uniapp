1ed1a96a5ee91d2928aa95e57dc54f2f7af2c2b9
mo3408 <2228293408@qq.com>
2021-02-04 14:13:05 +0800

    vant小程序uniapp版

---
+++ packages/tag/index.js
@@ -0,0 +1,17 @@
+Component({
+  externalClasses: ['custom-class', 'theme-class'],
+
+  properties: {
+    type: {
+      type: String,
+    },
+    plain: {
+      type: Boolean,
+      value: false,
+    },
+    disabled: {
+      type: Boolean,
+      value: false,
+    }
+  }
+});
